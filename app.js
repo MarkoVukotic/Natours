57b8e674122e9d0425265f5a97f2bfa8a0d22ffd
Marko Vukotice <markovukotic1411@gmail.com>
2022-06-26 15:50:42 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+//1) MIDDLEWARE
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+//2) ROUTE HANDLERS
+
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tours', tourRouter);
+
+module.exports = app;
