d3aea7010caf396e646ca2b3e3dca13629ef6679
marko.v <markovukotic32@gmail.com>
2022-09-25 15:28:13 +0200

    MVC Architecture, Mongoose, MongoDB, Models, Simple CRUD operations

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
 dotenv.config({path: './config.env'})
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 
-
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+}).then( () => {console.log('DB connection successfull')
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening on a port ${process.env.PORT}`);
