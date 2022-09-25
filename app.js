e9f592026da82c877f84bf129f1e99cba0b8424b
marko.v <markovukotic32@gmail.com>
2022-09-25 09:18:21 +0200

    Added ENV file and some middlewares

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //1) MIDDLEWARE
-app.use(morgan('dev'));
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 
 app.use((req, res, next) => {
